eef8570e692ee96adc2c0faecb27f9dbfbd5a27c
EyubEnsarYildirim <ensary6107@gmail.com>
2022-02-28 21:36:54 +0300

    fonkiyon örneği

---
+++ fonksiyon örneği.py
@@ -0,0 +1,7 @@
+sonuc=0;
+def toplama(sayi1,sayi2):
+    sonuc=sayi1 + sayi2
+    print("Fonksiyonun içinde toplam:",sonuc)
+    return sonuc
+toplama(5,15)
+print("Fonksiyonun dışında toplam:",sonuc)
